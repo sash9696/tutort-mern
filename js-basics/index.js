5c2ede425b938d9bb4d9ab2e639b85d435b232f9
Sahil Chopra <67851308+sash9696@users.noreply.github.com>
2024-06-22 11:22:01 +0530

    Add files via upload

---
+++ js-basics/index.js
@@ -0,0 +1,201 @@
+// "use strict";
+// console.log(200);
+
+
+
+//VARIABLES
+
+//named storage
+
+// let msg;
+// msg = 'hi there';
+
+// let msg = 'hi there';
+
+// console.log(msg)
+
+
+// let user  = 'john', age = 20; msg = 'Hello';
+
+
+// let msg;
+// msg = 'hi there';
+
+// msg = 200;
+
+// console.log(msg)
+
+// var msg = 'hello';
+
+// const msg = 'hello';
+
+
+//variable names
+
+// name must contain letters, digits, or $ and _ only
+//first chat should not be a digit
+
+// let username, user234, $, _
+
+
+// let 1ab,  my-name, return, let
+
+
+// num  = 10;
+
+// console.log(num)
+
+
+
+//const => unchanginf variable
+
+// const birthDay = '20.02.1996';
+// birthDay='dnjsnka'
+
+
+//uppercase contants
+
+//  const COLOR_RED =   '#FFOOO';
+
+//userName , shoppingCart
+
+
+//Basic data types
+//8
+//string, number , big int, boolean, null, undefined, Objects, symbols
+
+//dynamically typed
+
+
+
+//number
+
+// let n  =123;
+// n = 1.234
+
+// console.log(n)
+
+
+//special numeric values
+
+// console.log(10/0);
+
+// console.log(Infinity);
+
+// console.log(-Infinity);
+
+// console.log('one'/1);
+
+// console.log(NaN * 10)
+
+// console.log(NaN ** 0); //exception
+
+
+//Big int
+
+// (2^53-1) to  -(2^53-1) 
+
+// const bigNum = 346327846372648723842837648723648723648732643287463827n;
+
+
+
+//Strings
+
+// let name = 'john';
+// let name = "john";
+
+// let name = `john`;
+
+// let sum = 5;
+// console.log('the sum of 3 + 2 is: ',sum)
+
+// console.log(`the sum of 3 + 2 is: ${3 + 2} `);
+
+
+//Boolean
+
+// let isAdming = true;
+
+// let isLarger = 10 > 20;
+
+
+//null
+//nothing, empty, value is unknown
+
+// let age  = null;
+// console.log(age)
+
+
+//undefined
+
+//undefined => value is not defined
+//declare a valiable
+
+// var username;
+
+
+//Objects
+
+// let user = {
+//     name:'john',
+//     age:20,
+//     country:'india'
+// };
+
+// // console.log(user.age)
+// console.log(user["age"])
+
+// symbols are unique identifiers
+
+// const mySymbol = Symbol('test');
+
+// let user = {
+//     name:'john',
+//     age:20,
+//     country:'india',
+//     [mySymbol]:'value',
+//     'Symbol(test)':10,
+//     100:'abcde'
+// };
+
+// console.log(user);
+
+// can be used to create uniqu identifiers, hidden properties
+
+// console.log(user[mySymbol]);
+
+
+
+// const uniqueKey = Symbol('uniqueKey');
+
+
+// const obj = {
+
+//     [uniqueKey]: 100,
+
+// };
+
+// obj['uniqueKey']= 200;
+
+// // console.log(obj[uniqueKey]);
+// // console.log(obj['uniqueKey']);
+
+// for (let key in obj){
+//     console.log(key)
+// }
+
+
+let name = 'john', age = 20, country='india';
+
+
+
+// console.log('Name is :' + " " + name, 'Age is: '+age)
+
+
+// console.log(`My name is ${name} and age: ${age} and i live in ${country}`)
+
+
+// let user = null;
+
+
+
