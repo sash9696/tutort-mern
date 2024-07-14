3cf19537d52392e6b0a6912858230e143069582f
Sahil Chopra <67851308+sash9696@users.noreply.github.com>
2024-07-14 10:54:53 +0530

    Add files via upload

---
+++ test-react/BasicLocalReact/index.js
@@ -0,0 +1,4 @@
+//Components => break it down into components
+//each and every component will have its own data (state or props)
+//all the components are connected together and data flows through them
+
